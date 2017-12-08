a194e3257778938218b9923cb97b617c61c4d607
dengzhiqiang <1445124133@qq.com>
2017-12-08 16:57:26 +0800

    谷歌浏览器

---
+++ 常用浏览器代理/1.常用代理.js
@@ -0,0 +1,8 @@
+// 用户代理标题：
+// navigator.userAgent
+
+/*苹果代理*/
+// Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Mobile/9B176 MicroMessenger/4.3.2
+
+/*安卓代理*/
+// Mozilla/5.0 (Linux; U; Android 2.3.6; zh-cn; GT-S5660 Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1 MicroMessenger/4.5.255
