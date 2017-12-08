a4e158b63355b4f9af6785d80d9dc6e14c1a915b
dengzhiqiang <1445124133@qq.com>
2017-12-08 14:48:49 +0800

    require

--- rjsTest/buld.js
+++ rjsTest/buld.js
@@ -1,1 +1,21 @@
-define("app/d",["require","exports","module"],function(e,i,n){alert(1)}),define("app/c",["require","exports","module","app/d"],function(e,i,n){e("app/d")}),define("app/b",["require","exports","module","app/c"],function(e,i,n){e("app/c")}),define("app/a",["app/b"],function(e){return{name:1}}),define("lib/lib",["./lib"],function(){console.log("lib")}),require(["./app/a"],function(e){}),require(["./lib/lib"],function(e){}),define("app",function(){});
+define("app/d", ["require", "exports", "module"], function (e, i, n) {
+    alert(1)
+}),
+    define("app/c", ["require", "exports", "module", "app/d"], function (e, i, n) {
+        e("app/d")
+    }),
+    define("app/b", ["require", "exports", "module", "app/c"], function (e, i, n) {
+        e("app/c")
+    }),
+    define("app/a", ["app/b"], function (e) {
+        return {name: 1}
+    }),
+    define("lib/lib", ["./lib"], function () {
+        console.log("lib")
+    }),
+    require(["./app/a"], function (e) {
+    }),
+    require(["./lib/lib"], function (e) {
+    }),
+    define("app", function () {
+    });
--- touchmove滑动跟随手指/README.txt
+++
@@ -1,3 +0,0 @@
-手指跟随的触控滑动
-
-https://segmentfault.com/a/1190000003817077
