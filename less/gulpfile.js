235fc734543593abcb53a30dea96c9751948f10f
dengzhiqiang <1445124133@qq.com>
2017-12-09 11:31:13 +0800

    'less'

--- less/gulpfile.js
+++ less/gulpfile.js
@@ -4,9 +4,10 @@ var less = require('gulp-less');
 // 为什么gulp要安装两遍？
 // gulp实现文件更改自动刷新
 
+
 gulp.task('default', function () {
     // 将你的默认的任务代码放在这
-    gulp.src('./less/style.less')
+    gulp.src('./less/*.less')
         .pipe(less())
         .pipe(gulp.dest('./css/'));
 });
